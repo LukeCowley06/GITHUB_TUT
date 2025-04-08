f3ea14bc036847cb120751e09a85a3cd827a5d31
Luke Cowley <112928347+LukeCowley06@users.noreply.github.com>
2025-04-08 06:52:15 +0000

    Fixed area loop

--- calculate_area.py
+++ calculate_area.py
@@ -1,8 +1,8 @@
 # This script defines a function to calculate the area of a rectangle.
 def calculate_area(length, width):
-    area = lenght * width
+    area = length * width  # Fixed typo: 'lenght' -> 'length'
     return area
 
 # Testing
 output = calculate_area(5, 10)
-print("The calculated area is:" output)
+print("The calculated area is:", output)  # Fixed syntax error: added a comma
